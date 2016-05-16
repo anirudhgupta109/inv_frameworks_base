010faa113ad5160256064643bbc3152f6da00354
Nico60 <ndhouailly@gmail.com>
2016-05-16 21:48:30 +0200

    Add animations to qs tiles [1/2]
    Change-Id: I81e79cfccfc4565b1f7a6292b5f0b88be753df74

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4352,6 +4352,20 @@ public final class Settings {
         public static final String VIBRATION_ON_CHARGE_STATE_CHANGED = "vibration_on_charge_state_changed";
 
         /**
+         * Change quick settings tiles animation style
+         *
+         * @hide
+         */
+        public static final String ANIM_TILE_STYLE = "anim_tile_style";
+
+        /**
+         * Change quick settings tiles animation duration
+         *
+         * @hide
+         */
+        public static final String ANIM_TILE_DURATION = "anim_tile_duration";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -12134,3 +12148,4 @@ public final class Settings {
         return packages[0];
     }
 }
+
