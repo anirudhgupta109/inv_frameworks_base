26557fb6cd8803556c6e6121dbd311a1e7367acc
ezio84 <brabus84@gmail.com>
2018-01-19 15:30:10 +0100

    Ambient music: let hide clock and show track info in center screen [1/2]
    when a new track is played, allow to show track infos only,
    without the clock and notifications icons.
    In this case, also move track info to the screen center.
    
    This doesn't affect normal system ambient notifications, they'll keep
    clock and other stuff.
    Change-Id: I4b249464f17a1742f8926a48d63a82b11e3bc075
    
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -511,7 +511,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     private Ticker mTicker;
     private boolean mTicking;
 
-    private boolean mAmbientMediaPlaying;
+    private int mAmbientMediaPlaying;
 
     // Tracking finger for opening/closing.
     boolean mTracking;
@@ -730,7 +730,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 	}
 
     public void triggerAmbientForMedia() {
-        if (mAmbientMediaPlaying) {
+        if (mAmbientMediaPlaying != 0) {
             mDozeServiceHost.fireNotificationMedia();
         }
     }
@@ -6004,12 +6004,14 @@ public class StatusBar extends SystemUI implements DemoMode,
                         // Otherwise just show the always-on screen.
                         setPulsing(pulsingEntries);
                     }
+                    setForcedMediaPulse(mAmbientMediaPlaying == 2 ? reason : -1);
                 }
 
                 @Override
                 public void onPulseFinished() {
                     callback.onPulseFinished();
                     setPulsing(null);
+                    setForcedMediaPulse(-1);
                 }
 
                 private void setPulsing(Collection<HeadsUpManager.HeadsUpEntry> pulsing) {
@@ -6017,6 +6019,14 @@ public class StatusBar extends SystemUI implements DemoMode,
                     mNotificationPanel.setPulsing(pulsing != null);
                     mVisualStabilityManager.setPulsing(pulsing != null);
                     mIgnoreTouchWhilePulsing = false;
+                }
+
+                private void setForcedMediaPulse(int reason) {
+                    mNotificationPanel.setForcedMediaPulse(reason);
+                    mNotificationShelf.setForcedMediaPulse(reason);
+                    if (mAmbientIndicationContainer != null) {
+                        ((AmbientIndicationContainer)mAmbientIndicationContainer).setForcedMediaPulse(reason);
+                    }
                 }
             }, reason);
         }
@@ -6479,7 +6489,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     private void setForceAmbient() {
         mAmbientMediaPlaying = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.FORCE_AMBIENT_FOR_MEDIA, 0,
-                UserHandle.USER_CURRENT) == 1;
+                UserHandle.USER_CURRENT);
     }
 
     protected final ContentObserver mNavbarObserver = new ContentObserver(mHandler) {
